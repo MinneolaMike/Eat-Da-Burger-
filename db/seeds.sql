63f6f9181476e66097c9322f61d81cd412188cb4
chefgonzo <chefgonzo@gmail.com>
2018-08-25 11:18:20 -0400

    schema.sql & seeds.sql complete -- mysql database burgers_db created and populated

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+-- Table Values -- 
+
+INSERT INTO burgers (burger_name) VALUES ("Bacon Cheeseburger");
+INSERT INTO burgers (burger_name) VALUES ("Single Deluxe");
+INSERT INTO burgers (burger_name) VALUES ("Double Deluxe");
+INSERT INTO burgers (burger_name) VALUES ("Breakfast Burger");
